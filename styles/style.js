3a086253b933e8607e07d1a18df1acc33927881f
Zaiidmo <Zaiidmoumnii@gmail.com>
2023-10-20 11:31:47 +0100

    Making of Dropdown Menu

--- styles/style.js
+++ styles/style.js
@@ -1,6 +1,13 @@
+//Hamburger Menu
+
 const burger = document.querySelector(".burger");
 const navigation = document.querySelector(".navigation");
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   navigation.classList.toggle("active");
-})
+});
+
+//dropdown function
+function myDropDown() {
+  document.getElementById('dropdown').classList.toggle('show'); 
+}
