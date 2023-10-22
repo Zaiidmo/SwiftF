f82e862f3eba6be902156ef83f9b74b1654a6293
Zaiidmo <Zaiidmoumnii@gmail.com>
2023-10-22 18:41:38 +0100

    Adding a collapsible FAQ section

--- styles/style.js
+++ styles/style.js
@@ -11,3 +11,21 @@ burger.addEventListener("click", () => {
 function myDropDown() {
   document.getElementById('dropdown').classList.toggle('show'); 
 }
+
+//Collapsible FAQ 
+var coll = document.getElementsByClassName("collapse");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("coll");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
