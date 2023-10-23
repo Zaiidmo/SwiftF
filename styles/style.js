7da98eac3720ae762f075839bf8de23636bd1ac5
Zaiidmo <Zaiidmoumnii@gmail.com>
2023-10-23 16:37:06 +0100

    modal not completed yet

--- styles/style.js
+++ styles/style.js
@@ -42,3 +42,36 @@ service.addEventListener("click", function() {
       moreservices.style.display = "grid";
     }
   });
+
+  //Modal for Services 
+
+// var btn = document.querySelectorAll(".modal-btn");
+// var modal = document.querySelectorAll(".modal");
+// var span = document.querySelectorAll(".close");
+// var i;
+// for(i=0; i<modal.length; i++){
+//   btn[i].addEventListner("click", function(event) {
+//     modal[i].style.display = "block";
+//     console.log("it's working")
+//   })
+//   span[i].onclick = function(event) {
+//     modal[i].style.display = "none";
+//   }
+
+//   window.onclick = function(event) {
+//     if (event.target == modal[i]) {
+//       modal[i].style.display = "none";
+//     }
+//   }
+// }
+
+const dialog = document.querySelectorAll("dialog");
+const showButton = document.querySelectorAll("dialog + button");
+const closeButton = document.querySelectorAll("dialog button");
+var i;
+
+for(i=0; i<dialog.length; i++){
+  showButton[i].addEventListener("click", () => {
+    dialog.showModal();
+  });
+}
